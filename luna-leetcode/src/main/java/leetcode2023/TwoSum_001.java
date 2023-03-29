a5c665e5cd4f25d07c8030ff2e5a93f1d7cdc678
chenzhangyue <iszychen@gmail.com>
2023-03-29 19:34:26 +0800

    two sum

---
+++ luna-leetcode/src/main/java/leetcode2023/TwoSum_001.java
@@ -0,0 +1,41 @@
+package leetcode2023;
+
+import com.google.common.collect.Maps;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.HashMap;
+
+/**
+ * @author chenzhangyue
+ * 2023/3/29
+ */
+public class TwoSum_001 {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints[0] + "," + ints[1]);
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 1) {
+            return new int[]{-1, -1};
+        }
+        HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                Integer index = map.get(complement);
+                if (index != null) {
+                    return new int[]{i, index};
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+}
